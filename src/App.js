65fd54f2465fa40266221530dbee651788e373ee
Andrew Milner <95140821+milnera92@users.noreply.github.com>
2023-03-02 20:22:52 -0500

    more style

--- src/App.js
+++ src/App.js
@@ -7,64 +7,48 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
-
-  @media screen and (max-width: 768px) {
-    height: auto;
-    padding: 2rem;
-  }
 `;
 
 const Card = styled.div`
-  background-color: #FFB6C1;
-  border: 4px solid #000000;
-  box-shadow: 4px 4px #000000;
-  padding: 2rem;
-  margin: 2rem;
-  max-width: 80%;
+  background-color: #ff8080;
+  border: 8px solid black;
+  border-radius: 10px;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.5);
+  width: 80%;
+  padding: 1rem;
+  margin-bottom: 2rem;
 
-  @media screen and (max-width: 768px) {
-    max-width: 100%;
+  @media (max-width: 768px) {
+    width: 90%;
   }
 `;
 
 const Title = styled.h2`
   font-size: 2rem;
   margin-bottom: 1rem;
-  text-align: center;
-
-  @media screen and (max-width: 768px) {
-    font-size: 1.5rem;
-  }
 `;
 
 const Content = styled.p`
   font-size: 1.2rem;
   margin-bottom: 2rem;
-  text-align: center;
-
-  @media screen and (max-width: 768px) {
-    font-size: 1rem;
-  }
 `;
 
-const Button = styled.button`
-  background-color: #000000;
-  border: none;
-  color: #FFFFFF;
-  font-size: 1rem;
-  padding: 1rem 2rem;
+const PlayButton = styled.audio`
+  display: block;
+  margin: auto;
+`;
 
-  @media screen and (max-width: 768px) {
-    font-size: 0.8rem;
-    padding: 0.8rem 1.5rem;
-  }
+const Loading = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 2rem;
 `;
 
 function App() {
   const [randomItem, setRandomItem] = useState(null);
 
   useEffect(() => {
-    console.log('useEffect called');
     fetch('https://shuffle-be.onrender.com/rss')
       .then(response => response.json())
       .then(data => {
@@ -73,18 +57,20 @@ function App() {
   }, []);
 
   if (!randomItem) {
-    return <Container>Loading...</Container>;
+    return (
+      <Container>
+        <Loading>Loading...</Loading>
+      </Container>
+    );
   }
 
   return (
     <Container>
-      {randomItem && (
-        <Card>
-          <Title>{randomItem.title}</Title>
-          <Content>{randomItem.contentSnippet.substring(0, randomItem.contentSnippet.length - 91)}</Content>
-          <Button><audio src={randomItem.enclosure.url} controls></audio></Button>
-        </Card>
-      )}
+      <Card>
+        <Title>{randomItem.title}</Title>
+        <Content>{randomItem.contentSnippet.substring(0, randomItem.contentSnippet.length - 91)}</Content>
+        <PlayButton src={randomItem.enclosure.url} controls></PlayButton>
+      </Card>
     </Container>
   );
 }
