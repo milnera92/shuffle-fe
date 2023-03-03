1b212c89a448d1702dbdd7c5ad49500ad2fbafa9
Andrew Milner <95140821+milnera92@users.noreply.github.com>
2023-03-02 19:30:29 -0500

    fixing middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,26 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function(app) {
+//   app.use(
+//     '/rss',
+//     createProxyMiddleware({
+//       target: 'https://shuffle-be.onrender.com/rss',
+//       changeOrigin: true,
+//     })
+//   );
+// };
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
     '/rss',
     createProxyMiddleware({
-      target: 'https://shuffle-be.onrender.com/rss',
+      target: 'https://shuffle-be.onrender.com',
       changeOrigin: true,
+      pathRewrite: {
+        '^/rss': '/rss'
+      }
     })
   );
 };
